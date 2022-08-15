deed4b815833ccf8473e392b75eb33d071471fd8
Henri Silva <hpsilva+git@pm.me>
2022-08-15 02:47:40 -0300

    add relatorio 3 lab circuitos

---
+++ 4th semester/lab circuitos/Relatorio 3/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+float r1,
+    r2,
+    r3,
+    v1,
+    v2;
+
+r1 = 100000;
+r2 = 47000;
+r3 = 220000;
