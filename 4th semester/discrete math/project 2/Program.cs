84625410c422763e92dd1fa5ae844fcee41d6cea
Henri Silva <hpsilva+git@pm.me>
2022-10-13 20:39:30 -0300

    Add tests to discrete math project

--- 4th semester/discrete math/project 2/Program.cs
+++ 4th semester/discrete math/project 2/Program.cs
@@ -1,10 +1,14 @@
-﻿using System.Text;
+﻿using System.Numerics;
+using System.Text;
 
 // Probabilidade de erro em cada bit apos passar pela funcao NoiseItUp()
 float p = 0.01f;
 
 // Inicializa a lista de SNRdb com valores entre 3 e 10 em incrementos de 0.35, para obtermos 20 valores no total.
 List<float> SNRdb = new List<float>();
+
+// Inicializa a classe que vai rodar nossos testes.
+TestRunner testRunner = new TestRunner();
 
 for (int i = 0; i < 21; i++)
 {
@@ -22,86 +26,46 @@ for (int i = 0; i < SNRdb.Count; i++)
 // Cria o codificador de texto, que vai passar as mensagems de binario para string e vice versa.
 var codificador = new Codificador();
 
-// Classe que gera erros no codigo com a probabiidade p de dar flip em cada bit.
-var noise = new Noise();
-
-// const string textoParaCodificar =
-//     "Pra poesia que a gente nao vive, transformar o tedio em melodia.";
+// Texto a ser codificado
 const string textoParaCodificar =
     "Pra poesia que a gente nao vive, transformar o tedio em melodia.";
 
 var textoCodificado = codificador.TextoParaBinario(textoParaCodificar);
 
-// Relacao mensagem/codigo, R = (k/n)
-// float R = (1f / 5f);
-// for (int i = 0; i < SNR.Count; i++)
-// {
-//     Console.WriteLine("SNRdb: " + SNRdb[i]);
-//     // Cria a funcao erro que vai computar a probabilidade p de cada bit ser alterado.
-//     p = (float)ErrorFunction.Erfc(Math.Sqrt(2 * R * SNR[i]));
+// testRunner.TestarPorcentagemDeErroPorBitRepeticao(5, SNRdb, SNR, p, textoCodificado);
 
-//     int quantidadeDeErros = 0;
-//     int quantidadeDeIteracoes = 1000;
-//     // Console.WriteLine(p);
-//     for (int j = 0; j < quantidadeDeIteracoes; j++)
-//     {
-//         var repetitionCode = new RepetitionCode(textoCodificado, 5);
-//         repetitionCode.BinaryData = noise.NoiseItUp(repetitionCode.BinaryData, p);
-//         if (codificador.BinarioParaTexto(repetitionCode.DecodeToBinary()) != textoParaCodificar)
-//         {
-//             quantidadeDeErros++;
-//         }
-//     }
-//     //Console.WriteLine(p);
-//     Console.WriteLine((float)quantidadeDeErros / (float)quantidadeDeIteracoes);
-// }
+// testRunner.TestarPorcentagemDeErroPorBitHamming(7, 4, SNRdb, SNR, p, textoCodificado);
+
+testRunner.TestarPorcentagemDeErroPorBitSemCodigo(SNRdb, SNR, p, textoCodificado);
 
 
-// Cria o codigo de repeticao, com repeticao tamanho 5.
-var repetitionCode = new RepetitionCode(textoCodificado, 5);
+// // Cria o codigo de repeticao, com repeticao tamanho 5.
+// var repetitionCode = new RepetitionCode(textoCodificado, 5);
 
-// Cria o codigo de hamming com tamanho (16-1) = 15
-var hammingCode15 = new HammingCode(textoCodificado, 16);
+// // Cria o codigo de hamming com tamanho (16-1) = 15
+// var hammingCode15 = new HammingCode(textoCodificado, 16);
 
-// Cria o codigo de hamming com tamanho (8-1) = 7
-var hammingCode7 = new HammingCode(textoCodificado, 8);
+// // Cria o codigo de hamming com tamanho (8-1) = 7
+// var hammingCode7 = new HammingCode(textoCodificado, 8);
 
-textoCodificado = noise.NoiseItUp(textoCodificado, p);
-repetitionCode.BinaryData = noise.NoiseItUp(repetitionCode.BinaryData, p);
-hammingCode15.BinaryData = noise.NoiseItUp(hammingCode15.BinaryData, p);
-hammingCode7.BinaryData = noise.NoiseItUp(hammingCode7.BinaryData, p);
+// // Guardar as codificacoes originais antes de passarem pelo noise para podermos comparar depois.
+// var copiaRCBinaryData = repetitionCode.BinaryData;
+// var copiaHCBinaryData15 = hammingCode15.BinaryData;
+// var copiaHCBinaryData7 = hammingCode7.BinaryData;
 
-// Imprime os resultados
-Console.WriteLine("Resultado para o texto sem correcao de erros:");
-Console.WriteLine(codificador.BinarioParaTexto(textoCodificado));
-Console.WriteLine("Resultado do teste de repeticao R(5,1,3):");
-Console.WriteLine(codificador.BinarioParaTexto(repetitionCode.DecodeToBinary()));
-Console.WriteLine("Resultado do teste de hamming C(15,11,3):");
-Console.WriteLine(codificador.BinarioParaTexto(hammingCode15.DecodeToBinary()));
-Console.WriteLine("Resultado do teste de hamming C(11,7,3):");
-Console.WriteLine(codificador.BinarioParaTexto(hammingCode7.DecodeToBinary()));
+// var textoCodificadoNoiseado = noise.NoiseItUp(textoCodificado, p);
+// repetitionCode.BinaryData = noise.NoiseItUp(repetitionCode.BinaryData, p);
+// hammingCode15.BinaryData = noise.NoiseItUp(hammingCode15.BinaryData, p);
+// hammingCode7.BinaryData = noise.NoiseItUp(hammingCode7.BinaryData, p);
 
-// Decodificacao e recodificacao de imagem
 
-byte[] b = File.ReadAllBytes("sprite.png");
 
-string binaryImg = "";
-
-foreach (var item in b)
-{
-    binaryImg += Convert.ToString(item, 2).PadLeft(8, '0');
-}
-
-var hamCode = new HammingCode(binaryImg, 16);
-hamCode.BinaryData = noise.NoiseItUp(hamCode.BinaryData, p);
-
-var decoded = hamCode.DecodeToBinary();
-
-// convert binaryImg to byte array
-byte[] bytes = new byte[decoded.Length / 8];
-for (int i = 0; i < bytes.Length; i++)
-{
-    bytes[i] = Convert.ToByte(decoded.Substring(i * 8, 8), 2);
-}
-
-File.WriteAllBytes("sprite2.png", bytes);
+// // Imprime os resultados
+// Console.WriteLine("Resultado para o texto sem correcao de erros:");
+// Console.WriteLine(codificador.BinarioParaTexto(textoCodificado));
+// Console.WriteLine("Resultado do teste de repeticao R(5,1,3):");
+// Console.WriteLine(codificador.BinarioParaTexto(repetitionCode.DecodeToBinary()));
+// Console.WriteLine("Resultado do teste de hamming C(15,11,3):");
+// Console.WriteLine(codificador.BinarioParaTexto(hammingCode15.DecodeToBinary()));
+// Console.WriteLine("Resultado do teste de hamming C(11,7,3):");
+// Console.WriteLine(codificador.BinarioParaTexto(hammingCode7.DecodeToBinary()));
--- 4th semester/discrete math/project 2/TestRunner.cs
+++ 4th semester/discrete math/project 2/TestRunner.cs
@@ -1,1 +1,121 @@
-class TestRunner { }
+using System.Numerics;
+
+class TestRunner
+{
+    Noise noise = new Noise();
+
+    public void TestarPorcentagemDeErroPorBitRepeticao(
+        int n,
+        List<float> SNRdb,
+        List<float> SNR,
+        float p,
+        string textoCodificado,
+        int iteracoes = 100
+    )
+    {
+        var R = 1f / (float)n;
+        for (int i = 0; i < SNRdb.Count; i++)
+        {
+            Console.WriteLine("SNRdb: " + SNRdb[i]);
+            // Cria a funcao erro que vai computar a probabilidade p de cada bit ser alterado.
+            p = (float)ErrorFunction.Erfc(Math.Sqrt(2 * R * SNR[i]));
+
+            BigInteger quantidadeDeErros = 0;
+            BigInteger quantidadeDeIteracoes = 0;
+            //Console.WriteLine(p);
+
+            while (quantidadeDeErros <= iteracoes)
+            {
+                var rc = new RepetitionCode(textoCodificado, n);
+                rc.BinaryData = noise.NoiseItUp(rc.BinaryData, p);
+                var limpo = rc.DecodeToBinary();
+                for (int j = 0; j < limpo.Length; j++)
+                {
+                    if (limpo[j] != textoCodificado[j])
+                    {
+                        quantidadeDeErros++;
+                    }
+                    quantidadeDeIteracoes++;
+                }
+            }
+            //Console.WriteLine(p);
+            Console.WriteLine((double)quantidadeDeErros / (double)quantidadeDeIteracoes);
+        }
+    }
+
+    public void TestarPorcentagemDeErroPorBitHamming(
+        int n,
+        int k,
+        List<float> SNRdb,
+        List<float> SNR,
+        float p,
+        string textoCodificado,
+        int iteracoes = 100
+    )
+    {
+        var R = (float)k / (float)n;
+        for (int i = 0; i < SNRdb.Count; i++)
+        {
+            Console.WriteLine("SNRdb: " + SNRdb[i]);
+            // Cria a funcao erro que vai computar a probabilidade p de cada bit ser alterado.
+            p = (float)ErrorFunction.Erfc(Math.Sqrt(2 * R * SNR[i]));
+
+            BigInteger quantidadeDeErros = 0;
+            BigInteger quantidadeDeIteracoes = 0;
+            //Console.WriteLine(p);
+
+            while (quantidadeDeErros <= iteracoes)
+            {
+                var hc = new HammingCode(textoCodificado, (int)n + 1);
+                hc.BinaryData = noise.NoiseItUp(hc.BinaryData, p);
+                var limpo = hc.DecodeToBinary();
+                for (int j = 0; j < limpo.Length; j++)
+                {
+                    if (limpo[j] != textoCodificado[j])
+                    {
+                        quantidadeDeErros++;
+                    }
+                    quantidadeDeIteracoes++;
+                }
+            }
+            //Console.WriteLine(p);
+            Console.WriteLine((double)quantidadeDeErros / (double)quantidadeDeIteracoes);
+        }
+    }
+
+    public void TestarPorcentagemDeErroPorBitSemCodigo(
+        List<float> SNRdb,
+        List<float> SNR,
+        float p,
+        string textoCodificado,
+        int iteracoes = 100
+    )
+    {
+        var R = 1f;
+        for (int i = 0; i < SNRdb.Count; i++)
+        {
+            Console.WriteLine("SNRdb: " + SNRdb[i]);
+            // Cria a funcao erro que vai computar a probabilidade p de cada bit ser alterado.
+            p = (float)ErrorFunction.Erfc(Math.Sqrt(2 * R * SNR[i]));
+
+            BigInteger quantidadeDeErros = 0;
+            BigInteger quantidadeDeIteracoes = 0;
+            //Console.WriteLine(p);
+
+            while (quantidadeDeErros <= iteracoes)
+            {
+                var textoNoiseado = noise.NoiseItUp(textoCodificado, p);
+                for (int j = 0; j < textoNoiseado.Length; j++)
+                {
+                    if (textoNoiseado[j] != textoCodificado[j])
+                    {
+                        quantidadeDeErros++;
+                    }
+                    quantidadeDeIteracoes++;
+                }
+            }
+            //Console.WriteLine(p);
+            Console.WriteLine((double)quantidadeDeErros / (double)quantidadeDeIteracoes);
+        }
+    }
+}
