e1db5e77adf9bfe1da84cc429214a37d514c34b7
Hennrique da Silva <hpsilva@pm.me>
2023-04-17 17:25:00 -0300

    Add electromag exercises for the first exam

--- 5th semester/Circuits II/Relatorio 6/Relatorio6.py
+++ 5th semester/Circuits II/Relatorio 6/Relatorio6.py
@@ -6,6 +6,8 @@ eq1 = smp.Eq(-I1 + (V1-Va)/R1 + V1/R2, 0)
 eq2 = smp.Eq((Va-V1)/R1 + (Va-V2)/R1 + Va/R2, 0)
 eq3 = smp.Eq((V2-Va)/R1 + V2/R3 - I2, 0)
 
+smp.pprint(eq1)
+
 z11, z12, z21, z22 = smp.symbols('Z11 Z12 Z21 Z22')
 
 sols = smp.solve([eq1, eq2, eq3], [V1, V2, Va])
@@ -17,5 +19,6 @@ R3 = 82000
 
 z11 = (sols[V1]/I1)
 z11 = z11.subs([(I2, 0)])
+z12 = (sols[V1]/I2)
 
 smp.pprint(z11)
