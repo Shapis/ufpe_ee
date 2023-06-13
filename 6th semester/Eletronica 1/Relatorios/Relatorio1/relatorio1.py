025f600f5c5749c96ca2c3a0c27bb396ffbb1481
Hennrique da Silva <hpsilva@pm.me>
2023-06-13 07:20:23 -0300

    Update relatorio 1 eletronica 1

--- 6th semester/Eletronica 1/Relatorios/Relatorio1/relatorio1.py
+++ 6th semester/Eletronica 1/Relatorios/Relatorio1/relatorio1.py
@@ -2,8 +2,8 @@ import sympy as smp
 from sympy import *
 
 # Definindo as variáveis simbólicas
-Vo, Vi, Va, Vc, R1, R2, Rp, Cp, A, w, j = smp.symbols(
-    'V_o2 V_i V_a V_c R_1 R_2 R_p C_p A w j')
+Vo, Vi, Va, Vc, R1, R2, Rp, Cp, A, w, j, Hs = smp.symbols(
+    'V_o V_i V_a V_c R_1 R_2 R_p C_p A w j H_s')
 
 # Analise nodal do circuito
 
@@ -12,6 +12,17 @@ eq2 = smp.Eq(Va * ((1/(j * w * Cp)))/(((1/(j * w * Cp))) + Rp), -Vc)
 eq3 = smp.Eq(A * Vc, Vo)
 
 
-solution = smp.solve([eq1, eq2, eq3], [Va, Vc, Vo])
+sols = smp.solve([eq1, eq2, eq3], [Va, Vc, Vo])
 
-smp.pprint(solution[Vo])
+# Aqui fazemos a seguinte substituição:
+# Rp >> R1 , Rp >> R2, e A >> 1
+
+eqVo = (-A * R2 * Vi)/((Rp*(R1 + R2))*j*w*Cp + A*R1)
+
+Hs = eqVo / Vi
+
+# Agora vamos isolar j*w no denominador.
+
+Hs = (-(A*R2)/(Cp*Rp*(R1+R2))) / (A*R1/(Cp*Rp*(R1+R2)) + j*w)
+
+smp.pprint(Hs)
