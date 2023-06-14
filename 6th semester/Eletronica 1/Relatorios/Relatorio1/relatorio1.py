a1891c104045de66fc4bb63dac59829df7e3ae6f
Hennrique da Silva <hpsilva@pm.me>
2023-06-14 11:40:47 -0300

    Add updates to relatorio 1 eletronica 1

--- 6th semester/Eletronica 1/Relatorios/Relatorio1/relatorio1.py
+++ 6th semester/Eletronica 1/Relatorios/Relatorio1/relatorio1.py
@@ -1,9 +1,9 @@
 import sympy as smp
-from sympy import *
+# from sympy import I
 
 # Definindo as variáveis simbólicas
-Vo, Vi, Va, Vc, R1, R2, Rp, Cp, A, w, j, Hs = smp.symbols(
-    'V_o V_i V_a V_c R_1 R_2 R_p C_p A w j H_s')
+Vo, Vi, Va, Vc, R1, R2, Rp, Cp, A, w, j, Hjw, wp, wc, K = smp.symbols(
+    'V_o V_i V_a V_c R_1 R_2 R_p C_p A w j H_jw w_p, w_c, K')
 
 # Analise nodal do circuito
 
@@ -17,12 +17,31 @@ sols = smp.solve([eq1, eq2, eq3], [Va, Vc, Vo])
 # Aqui fazemos a seguinte substituição:
 # Rp >> R1 , Rp >> R2, e A >> 1
 
-eqVo = (-A * R2 * Vi)/((Rp*(R1 + R2))*j*w*Cp + A*R1)
+Vo = (-A * R2 * Vi)/((Rp*(R1 + R2))*j*w*Cp + A*R1)
 
-Hs = eqVo / Vi
+eqHjw = smp.Eq(Hjw, Vo/Vi)
 
-# Agora vamos isolar j*w no denominador.
+# Resolvendo a equacao Hjw e colocando no formato canonico da um fitro passa baixa (-K wp / (jw + wp)) obtemos o seguinte:
 
-Hs = (-(A*R2)/(Cp*Rp*(R1+R2))) / (A*R1/(Cp*Rp*(R1+R2)) + j*w)
+eqwp = smp.Eq(wp, 1/(Rp*Cp))
 
-smp.pprint(Hs)
+eqK = smp.Eq(K, R2/R1)
+
+eqwc = smp.Eq(wc, (A*wp)/(1 + K))
+
+
+# Exemplo 1
+
+print("Exemplo 1")
+print("Para R1 = 4.7E4 ohms, R2 = 2.2E5 ohms, wp = 2E1 pi e A = 1E5")
+
+eqK1 = eqK.subs({R1: 4.7E4, R2: 2.2E5})
+# smp.pprint(eqK)
+smp.pprint(eqK1.evalf())
+
+K1 = smp.solve(eqK1, K)[0]
+
+smp.pprint(eqwc)
+smp.pprint(eqwc.subs({A: 1E5, wp: 20 * smp.pi, K: K1}).evalf())
+
+
