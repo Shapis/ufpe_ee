b668164cc7d94d739c437ebd066d72e2a3b409b6
Henrique da Silva <hpsilva@pm.me>
2024-02-23 17:59:45 -0300

    dasdsa

---
+++ 7th Semester/PDS/Filtro de Voz/bruno.py
@@ -0,0 +1,13 @@
+# Eu realmente odeio essa linguagem
+
+x = 0
+y = 0
+
+while True:
+    x += 1
+    y += 1
+    if x is y:
+        print(f"X: {x} Y: {y} , sao iguais!")
+    else:
+        print(f"X: {x} Y: {y} , sao diferentes!")
+        break
