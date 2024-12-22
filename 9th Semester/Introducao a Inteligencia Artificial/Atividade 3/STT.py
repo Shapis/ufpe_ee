ef189d56cae728c8308053a6cef06179ea45bd77
Henrique da Silva <hpsilva@pm.me>
2024-12-22 18:55:49 -0300

    Fix atividade 3 intro to ai

--- 9th Semester/Introducao a Inteligencia Artificial/Atividade 3/STT.py
+++ 9th Semester/Introducao a Inteligencia Artificial/Atividade 3/STT.py
@@ -1,13 +1,13 @@
 import speech_recognition as sr
 from pydub import AudioSegment
 
-audio_file = "camoes.wav"
-sound = AudioSegment.from_mp3(audio_file)
-sound.export("audio_convertido.wav", format="wav")
+# audio_file = "camoes.wav"
+# sound = AudioSegment.from_mp3(audio_file)
+# sound.export("audio_convertido.wav", format="wav")
 
 recognizer = sr.Recognizer()
 
-with sr.AudioFile("audio_convertido.wav") as source:
+with sr.AudioFile("camoes.wav") as source:
     audio = recognizer.record(source)
 
 try:
