d223419bfee7a6b7483c67fd1a5622454ba60047
Henri da Silva <hpsilva@pm.me>
2025-06-29 09:05:07 -0300

    Update assembly information and project configuration files

--- 6th semester/Servomecanismos/Projeto Carro/.godot/mono/temp/obj/Debug/Servocarro.AssemblyInfo.cs
+++ 6th semester/Servomecanismos/Projeto Carro/.godot/mono/temp/obj/Debug/Servocarro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Servocarro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de758d0c015630dfc1835d60752ea5512232c241")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a8b6d0f79d3c68b16fafdcd78f22e7514445be2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Servocarro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Servocarro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
